4517ea2611090047403801f53d1f8e70d825a719
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2022-08-01 09:05:51 -0400

    ....

--- app/socket.py
+++ app/socket.py
@@ -12,7 +12,7 @@ if os.environ.get('FLASK_ENV') == 'production':
 else:
   origins = '*'
 
-socketio = SocketIO(cors_allowed_origins='*', allowEIO3: True)
+socketio = SocketIO(allowEIO3: True, cors_allowed_origins='*')
 
 @socketio.event
 def connect():
